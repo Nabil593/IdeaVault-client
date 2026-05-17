f21e63b465f8af004796e0bf1d14e472f7a094b7
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-05-18 00:16:16 +0600

    project initia lsetup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./Components/shared/Navbar";
+import Footer from "./Components/shared/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,8 +23,15 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      cz-shortcut-listen="true"
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        <mian className="flex-1">
+         {children}
+        </mian>
+        <Footer />
+      </body>
     </html>
   );
 }
